dac98ba5cf0044ff5ed551bb8a37c7f103febdc9
Enoch Canlas <ronenochcanlas@gmail.com>
2016-11-13 16:50:38 -0800

    Added 2 exercise files

---
+++ ex2.py
@@ -0,0 +1,9 @@
+#This is a comment
+#Python ignores text after the pound sign
+
+print "I could have coded like this" # and the comment after is ignored
+
+# You can also comment to "disable" or comment out a piece of code:
+#print "This won't run"
+
+print "This will run"
